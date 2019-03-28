2245eec1592d8c16363cac5464a86e111e4ac60b
Jonas Hagen <jonas.hagen@iap.unibe.ch>
2019-03-28 14:34:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,16 @@ setup(
         "Programming Language :: Python 3.6",
     ],
     description="Periodically retrieve data from different sources.",
-    license="mit",
+    license="MIT",
     maintainer="Jonas Hagen",
     maintainer_email="jonas.hagen@iap.unibe.ch",
     name="databird",
     packages=["databird", "databird_drivers.standard"],
     python_requires=">=3.5.*, <4",
     version=VERSION,
+    url="https://github.com/jonas-hagen/databird",
+    download_url="https://github.com/jonas-hagen/databird/archive/databird-{v}.tar.gz".format(
+        v=VERSION
+    ),
     install_requires=["ruamel.yaml", "dict-recursive-update", "frozendict", "mr.bob"],
 )
